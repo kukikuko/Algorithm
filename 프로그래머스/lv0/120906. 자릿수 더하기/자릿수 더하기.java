1de880b9b26639d73c626bcf229aa1ebc162de95
kukikuko <103628608+kukikuko@users.noreply.github.com>
2023-01-01 17:49:03 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 65.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String s = String.valueOf(n);
+		for(int i=0; i<s.length(); i++) {
+			answer += Integer.parseInt(String.valueOf(s.charAt(i)));
+		}
+        return answer;
+    }
+}
