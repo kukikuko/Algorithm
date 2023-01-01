e4a2e6cd9bf7171d226eaf730059a3c4c3bda87f
kukikuko <103628608+kukikuko@users.noreply.github.com>
2023-01-01 18:07:15 +0900

    [level 1] Title: 음양 더하기, Time: 0.05 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int sum = 0;
+		for(int i=0; i<absolutes.length; i++) {
+			
+			if(signs[i] == true) {
+				sum += absolutes[i];
+			} else {
+				sum = sum -  absolutes[i];
+			}
+			
+		}
+        
+        return sum;
+    }
+}
