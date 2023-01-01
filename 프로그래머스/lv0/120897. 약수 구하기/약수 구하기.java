78385316ffa299099a864c66c4f28e79621a2d1e
kukikuko <103628608+kukikuko@users.noreply.github.com>
2023-01-01 17:50:21 +0900

    [level 0] Title: 약수 구하기, Time: 0.02 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+    public List solution(int n) {
+        List<Integer> answer = new ArrayList<Integer>();
+		for(int i=1; i<=n; i++) {
+			if(n%i==0) answer.add(i);
+		}
+        return answer;
+    }
+}
