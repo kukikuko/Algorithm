429143d24198e519efdfffe89f7d3b6ab66dfb1e
kukikuko <103628608+kukikuko@users.noreply.github.com>
2023-01-01 17:48:05 +0900

    [level 0] Title: 7의 개수, Time: 0.03 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        for(int i : array) {
+			String s = String.valueOf(i);
+			for(int j=0; j<s.length(); j++) {
+				if('7' == s.charAt(j)) answer++;
+			}
+		}
+        return answer;
+    }
+}
