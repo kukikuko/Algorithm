ec60ed4a8e2b839bacbf63e5aa440ee3693a0700
kukikuko <103628608+kukikuko@users.noreply.github.com>
2023-01-22 22:52:08 +0900

    [level 2] Title: 올바른 괄호, Time: 13.32 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,22 +1,23 @@
+import java.util.Stack;
 class Solution {
     boolean solution(String s) {
         boolean answer = false;
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        int cnt = 0;
+        Stack<String> st = new Stack<>();
 		char[] arr = s.toCharArray();
+		
 		for(char c : arr) {
-			if(c == '(') cnt += 1;
-			else cnt -= 1;
-			
-			if(cnt < 0) {
-				return answer;
+			try {
+			if(c == '(') st.add("(");
+			else st.pop();
+			} catch (Exception e) {
+				answer = false;
+                return answer;
 			}
 		}
-	
-		if(cnt == 0) {
-			answer = true;
-		}
+		
+		if(st.empty()) answer = true;
 
         return answer;
     }
