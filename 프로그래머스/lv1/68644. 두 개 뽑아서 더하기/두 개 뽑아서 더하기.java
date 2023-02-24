75706579ec0213c6e8cf96c05dbeea8cd01547ea
kukikuko <103628608+kukikuko@users.noreply.github.com>
2023-02-24 14:15:46 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.95 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public List solution(int[] numbers) {
+        List<Integer> answer = new ArrayList<>();
+		for(int i=0; i<numbers.length-1; i++) {
+			for(int j=i+1; j<numbers.length; j++) {
+				if(!answer.contains(numbers[i] + numbers[j]))
+				answer.add(numbers[i] + numbers[j]);
+			}
+		}
+		Collections.sort(answer);
+        
+        return answer;
+    }
+}
