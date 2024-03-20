23d114a9880add4f8283b0dc45351d0f1be1be54
kukikuko <103628608+kukikuko@users.noreply.github.com>
2024-03-20 20:04:05 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.04 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -2,9 +2,11 @@ class Solution {
     public String solution(String my_string, int k) {
         String answer = "";
         
-        for(int i = 0; i < k; i++) {
-            answer += my_string;
-        }
+        answer = my_string.repeat(k);
+        
+        // for(int i = 0; i < k; i++) {
+        //     answer += my_string;
+        // }
         
         return answer;
     }
