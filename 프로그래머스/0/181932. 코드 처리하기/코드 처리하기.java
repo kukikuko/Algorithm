a1433dd181b8a2118311b71e34163457ab7df375
kukikuko <103628608+kukikuko@users.noreply.github.com>
2024-09-09 10:33:51 +0900

    [level 0] Title: 코드 처리하기, Time: 459.68 ms, Memory: 381 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        char[] arr = code.toCharArray();
+        int mode = 0;
+        
+        for(int i=0; i<arr.length; i++) {
+            if(mode == 0) {
+                if(arr[i] == '1') mode = 1;
+                else if(i % 2 == 0) answer += arr[i];
+            } else {
+                if(arr[i] == '1') mode = 0;
+                else if(i % 2 != 0) answer += arr[i];
+            }
+        }
+        
+        if(answer.equals("")) answer = "EMPTY";
+        
+        return answer;
+    }
+}
