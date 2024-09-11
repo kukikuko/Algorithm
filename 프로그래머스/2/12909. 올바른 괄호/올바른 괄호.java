4a09dc0b2d12a18b4d92db12b34579ccb1864f08
kukikuko <103628608+kukikuko@users.noreply.github.com>
2024-09-11 16:09:25 +0900

    [level 2] Title: 올바른 괄호, Time: 6.38 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,16 +1,17 @@
 class Solution {
     boolean solution(String s) {
+        boolean answer = false;
         int cnt = 0;
         
         for(int i=0; i<s.length(); i++) {
             if(s.charAt(i) == '(') cnt++;
             else cnt--;
             
-            if(cnt < 0) return false;
+            if(cnt < 0) break;
         }
         
-        if(cnt > 0) return false;
+        if(cnt == 0) answer = true;
 
-        return true;
+        return answer;
     }
 }
