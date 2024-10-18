63e681bd89f171bd587541ef16f55b04d5839faf
kukikuko <103628608+kukikuko@users.noreply.github.com>
2024-10-18 14:00:37 +0900

    [level 0] Title: 5명씩, Time: 0.04 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -1,12 +1,9 @@
 class Solution {
     public String[] solution(String[] names) {
-        int index = names.length%5 == 0 ? names.length / 5 : names.length / 5 + 1;
-        String[] answer = new String[index];
-        
+        String[] answer = new String[(names.length-1)/5+1];
         for(int i=0; i<answer.length; i++) {
             answer[i] = names[i * 5];
         }
-        
         return answer;
     }
 }
