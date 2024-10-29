b6ae48073b3af004352744dec1719acbc12af717
kukikuko <103628608+kukikuko@users.noreply.github.com>
2024-10-29 09:14:43 +0900

    [level 0] Title: ad 제거하기, Time: 2.67 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -2,20 +2,12 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] strArr) {
-        Queue<String> qu = new LinkedList<>();
+        List<String> list = new ArrayList<>();
         
         for(String s : strArr) {
-            if(!s.contains("ad")) {
-                qu.offer(s);
-                System.out.println(s);
-            }
+            if(!s.contains("ad")) list.add(s);
         }
         
-        String[] answer = new String[qu.size()];
-        for(int i=0; i<answer.length; i++) {
-            answer[i] = qu.poll();
-        }
-        
-        return answer;
+        return list.toArray(String[]::new);
     }
 }
