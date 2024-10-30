5a8c3323c2f2f771542d787d0b4aae9b4394071d
kukikuko <103628608+kukikuko@users.noreply.github.com>
2024-10-30 17:21:14 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 1.91 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for(int[] ar : queries) {
+            for(int i=ar[0]; i<=ar[1]; i++){
+                arr[i] += 1;
+            }
+        }
+        
+        return arr;
+    }
+}
