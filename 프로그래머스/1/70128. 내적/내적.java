7bffa36d4c6a203f9831972281085f3e63273966
kukikuko <103628608+kukikuko@users.noreply.github.com>
2024-11-25 15:37:23 +0900

    [level 1] Title: 내적, Time: 0.06 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -1,10 +1,9 @@
 class Solution {
     public int solution(int[] a, int[] b) {
         int answer = 0;
-	for(int i=0; i<a.length; i++) {
-		if(a[i] != 0 && b[i] != 0) 
+	    for(int i=0; i<a.length; i++) {
 			answer += (a[i] * b[i]);
-	}
+	    }
         return answer;
     }
 }
