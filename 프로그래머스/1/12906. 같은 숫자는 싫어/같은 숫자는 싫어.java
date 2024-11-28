22b74c565b2f7c13e195cd3df5aa439cdbe43b6b
kukikuko <103628608+kukikuko@users.noreply.github.com>
2024-11-28 17:36:30 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.70 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -10,7 +10,12 @@ public class Solution {
                 index = i;
             }
         }
+        
+        int[] answer = new int[list.size()];
+        for(int i=0; i<answer.length; i++){
+            answer[i] = list.get(i);
+        }
 
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
     }
 }
