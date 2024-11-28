a460a4b53792efdb45ba294e9c7eb332586918ff
kukikuko <103628608+kukikuko@users.noreply.github.com>
2024-11-28 11:01:21 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.13 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,7 +1,5 @@
 class Solution {
     public boolean solution(String s) {
-       	if((s.length() == 4 || s.length() == 6) && s.matches("[0-9]+"))
-            return true;
-        return false;
+       	return (s.length() == 4 || s.length() == 6) && s.matches("[0-9]+");
     }
 }
