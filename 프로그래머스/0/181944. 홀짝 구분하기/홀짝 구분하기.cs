0683a630dbfe5f5b0242d6ad4eadb21da66dd9b0
kukikuko <103628608+kukikuko@users.noreply.github.com>
2025-09-09 11:53:29 +0900

    [level 0] Title: 홀짝 구분하기, Time: 20.48 ms, Memory: 16.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String[] s;
+
+        Console.Clear();
+        s = Console.ReadLine().Split(' ');
+
+        int a = Int32.Parse(s[0]);
+        string result = a % 2 == 0 ? "even" : "odd";
+        
+        Console.Write($"{a} is {result}");
+    }
+}
