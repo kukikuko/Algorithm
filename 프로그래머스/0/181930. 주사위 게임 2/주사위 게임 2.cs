f3d79eb059cc0b58fce4a72ed3cae944a311156b
kukikuko <103628608+kukikuko@users.noreply.github.com>
2025-09-11 12:00:15 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.19 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cs
@@ -0,0 +1,36 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        
+        if (a == b)
+        {
+        if (a == c)
+        {
+            answer += (a + b + c) * (a * a + b * b + c * c) * 
+                    (a * a * a + b * b * b + c * c * c);
+        }
+        else
+        {
+            answer += (a + b + c) * (a * a + b * b + c * c);
+        }
+        }
+        else
+        {
+            if (b == c)
+            {
+                answer += (a + b + c) * (a * a + b * b + c * c);
+            }
+            else if (a == c) 
+            {
+                answer += (a + b + c) * (a * a + b * b + c * c);
+            }
+            else
+            {
+                answer += a + b + c;
+            }
+        }
+        return answer;
+    }
+}
