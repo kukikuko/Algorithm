ce3d6dbc437c80949958e1979979530f7a63a6e5
kukikuko <103628608+kukikuko@users.noreply.github.com>
2026-01-02 09:58:28 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 1.47 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers) {
+        Array.Sort(numbers);
+        Array.Reverse(numbers);
+        int answer = numbers[0] * numbers[1];
+        return answer;
+    }
+}
